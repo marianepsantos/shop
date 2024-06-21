bb6e7a639332e0bec74214c336da11d7aa51dca5
marianepsantos <marianepsantos15@gmail.com>
2024-06-21 16:02:47 -0400

    Commit
    21/06/2024

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,15 +1,35 @@
 import Image from "next/image";
 import styles from "./main.module.css";
+import { useState } from "react";
 
 
 export default async function Main() {
   const response = await fetch("https://fakestoreapi.com/products/");
   const data = await response.json();
+  const [listProduct, setListProduct] = useState([]);
+  
+
+
+  const searchText = (text) => {
+    setSearch(text);
+
+    if(text.trim() == "" ){
+      setListProduct(listComplete);
+      return
+    }
+    const newList = listProduct.filter((product) =>
+      product.title.toUpperCase().trim().includes(search.toUpperCase))
+    setListProduct(newList);
+}
+
 
   return (
     <main className={styles.main}>
       <h2>Produtos</h2>
       <div className={styles.produto}>
+        <div>
+      
+        </div>
         {produto.map((produto) => (
           <div className={styles.produto}key= {produto.id}>
             <p>{products.title}</p>
